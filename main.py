551b5220196c226b7210ecb773c711e4c82e51eb
LukasMoritz2707 <140209117+LukasMoritz2707@users.noreply.github.com>
2024-06-25 14:49:16 +0200

    update main.py and add examplepackage.sty
    Main is now ready to use, but currently not with options, so it currently only detects all packages but not the options
    
    It runs a full loop which stops when their are no more packages that can be found
    
    The examplepackage.sty is a simple package that just loads another package however it is not necessary to test the code, if the \usepackage{examplepackage} is deleted, or commeted out in the example.tex

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 ## Import Statements
 import sys
 import os
+import re
 
 
 class PackageClass:                                                                                                     ## Class to create Parents and know which Children are connected to the parent
 
-    def __init__(self, name, parent):
+    def __init__(self, name, path, parent):
         self.name = name
         self.parent = parent
+        self.path = path
         self.childlist = []
         self.childpaths = []
 
@@ -19,20 +21,40 @@ class PackageClass:                                                             
         self.childpaths.append(findfile(items + ".sty", "/usr/share/texlive/texmf-dist/tex"))
 
 
-    def printchild(self):
-        for items in self.childlist:
-            print(items)
+#    def printchild(self):
+#        for items in self.childlist:
+#            print(items)
 
+    def setchildlist(self, list):
+        self.childlist = list
+        for items in self.childlist:
+            self.childpaths.append(findfile(items + ".sty", "/usr/share/texlive/texmf-dist/tex"))
+
+#    def getchild(self, index):
+#        return self.childlist[index]
+
+    def getchildpath(self, index):
+        return self.childpaths[index]
+
+    def getpath(self):
+        return self.path
 
     def tostring(self):
         print("Name: ", self.name, "\n"
               "Parent: ", self.parent, "\n"
+              "Path: ", self.path, "\n"                        
               "Childlist: ", self.childlist, "\n"
               "Childpaths: ", self.childpaths, "\n")
 
 
+    def childlistempty(self):
+        if len(self.childlist) == 0:
+            return True
+        else:
+            return False
+
 def helpmessage():                                                                                                      ## Displays helpmessage if no arguments are provided
-    print("Usage: ParserforDependencies.py [OPTIONS] [SEEDS] \n"
+    print("Usage:", sys.argv[0], "[OPTIONS] [SEEDS] \n"
           "\n"
           "This skript will parse a LaTeX seed file and parse which \n" 
           "other files they require. Dependent files will only be \n" 
@@ -57,8 +79,14 @@ def outfile(arguments):                                                         
         return "graph.tex"
 
 
-def loadseedfile(seedfile):                                                                                             ## Loads the seedfile and finds the oackages loaded in the file
-    filepath = os.path.abspath(seedfile)
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+def loadseedfile(seedfile, pathgiven= False):                                                                           ## Loads the seedfile and finds the oackages loaded in the file
+    if pathgiven == False:
+        filepath = os.path.abspath(seedfile)
+    else:
+        filepath = seedfile
 
     with open(filepath, "r") as file:
         texfile = []
@@ -79,15 +107,28 @@ def loadseedfile(seedfile):                                                     
                 break
 
         packages = []
+        pattern = r'\{(.*?)\}'
 
         for items in texfile:
-            packages.append(items.strip("\\ \n usepackage RequirePackage RequirePackageWithOptions"))
+            packages.append(re.findall(pattern, items))
 
-        for n in range(0, len(packages)):
-            packages[n] = packages[n].strip("{ }")
+        packages = flatten(packages)
 
-        return packages
+        check_list = []
+        for items in packages:
+            if "," in items:
+                parts = items.split(",")
+                check_list.extend(parts)
+            else:
+                check_list.append(items)
 
+        return check_list
+
+def findchild(Parent):
+    parentpath = Parent.getpath()
+    children = loadseedfile(parentpath, True)
+
+    return children
 
 def findfile(filename, path):                                                                                           ## Function used to find the file path for later use
     for root, dirs, files in os.walk(path):
@@ -116,18 +157,42 @@ if __name__ == '__main__':
             print("Something went wrong. Try again (Index was out of bounce, missing outfile name?)")
             sys.exit(2)
 
-    parent = PackageClass(seedfilename, None)                                                                     ## Creates main parent
+    currentdirectory = os.getcwd()
+    parent = PackageClass(seedfilename, currentdirectory + "/" + seedfilename ,None)                             ## Creates main parent
 
     for items in seedfile:                                                                                              ## adds children with their given paths
         parent.addchild(items)
 
     childs = []
 
-    for items in parent.childlist:                                                                                      ## Creates child opjects
-        child = PackageClass(items, parent.name)
+    for i, items in enumerate(parent.childlist):                                                                        ## Creates child opjects
+        child = PackageClass(items, parent.getchildpath(i),parent.name)
         childs.append(child)
 
-    parent.tostring()
+    for items in childs:
+        children = findchild(items)
+        items.setchildlist(children)
 
     for items in childs:
-        items.tostring()
+        if items.childlistempty():
+            print("No further packages requierd by ", items.name)
+        else:
+            print("Further packages requierd by ", items.name)
+            for i, children in enumerate(items.childlist):
+                child = PackageClass(children, items.getchildpath(i), items.name)
+                childrenofchild = findchild(child)
+                child.setchildlist(childrenofchild)
+                childs.append(child)
+
+
+    print("\n "
+          "All packages requierd by", seedfilename, "should be found. \n"
+          "\n"
+          "Inputfile has the following packages: \n",
+          parent.childlist, "\n"
+          "\n"
+          "Other packages that are needed are: \n")
+    for items in childs:
+        if not items.childlistempty():
+            print("- Package", items.name ,"requiers: ",
+                items.childlist)
