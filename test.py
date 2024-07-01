081c7663981ce98a5d881085f862a32fdee705d3
LukasMoritz2707 <140209117+LukasMoritz2707@users.noreply.github.com>
2024-07-01 15:39:52 +0200

    update test.py
    add a new test for the browser flag and add QoL features

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ import os
 import unittest
 import subprocess
 
+
+class Bcolors:
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
 class TestParserforDependencis(unittest.TestCase):
 
     def runprogram(self, args):
@@ -22,10 +29,10 @@ class TestParserforDependencis(unittest.TestCase):
         print(f"Running test: {functionname}")
         try:
             result = self.runprogram([])
-            self.assertEqual(result.returncode, 2)
-            print(f"{functionname}: SUCCESS")
+            self.assertEqual(result.returncode, 1)
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test without outfile flag set but no argument given
@@ -36,9 +43,9 @@ class TestParserforDependencis(unittest.TestCase):
             result = self.runprogram(["-outfile"])
             self.assertEqual(result.returncode, 2)
             self.assertIn("ParserforDependenciesV2.py [-h] [-outfile OUTFILENAME] [-s SOURCEFILE]\n", result.stderr)
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test without sourcefile flag set but no argument given
@@ -49,23 +56,22 @@ class TestParserforDependencis(unittest.TestCase):
             result = self.runprogram(["-s"])
             self.assertEqual(result.returncode, 2)
             self.assertIn("ParserforDependenciesV2.py: error: argument -s/--sourcefile: expected one argument", result.stderr)
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
-
 
     # Test without outfile and sourcefile flag set but no arguments given
     def test_withoutsorcefileandoutfileargument(self):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-outfile -s"])
+            result = self.runprogram(["-outfile", "-s"])
             self.assertEqual(result.returncode, 2)
             self.assertIn("usage: ParserforDependenciesV2.py [-h] [-outfile OUTFILENAME] [-s SOURCEFILE]", result.stderr)
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test without sourcefile flag set and file given
@@ -73,12 +79,12 @@ class TestParserforDependencis(unittest.TestCase):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-s example.tex"])
-            self.assertEqual(result.returncode, 1)
+            result = self.runprogram(["-s", "example.tex"])
+            self.assertEqual(result.returncode, 0)
             self.assertTrue(os.path.isfile("graph.html"))
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test without outfile (no argument) and sourcefile flag set and file given
@@ -86,24 +92,25 @@ class TestParserforDependencis(unittest.TestCase):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-outfile -s example.tex"])
+            result = self.runprogram(["-outfile", "-s", "example.tex"])
             self.assertEqual(result.returncode, 2)
             self.assertIn("usage: ParserforDependenciesV2.py [-h] [-outfile OUTFILENAME] [-s SOURCEFILE]", result.stderr)
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
+
     # Test with sourcefile and outfile flag set and arguments given for both (outfile with .html)
     def test_withsourcefileandoutfileflagandargumentswihthtml(self):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-outfile testgraph.html -s example.tex"])
-            self.assertEqual(result.returncode, 2)      # Test bekommt 2 zurück, warum?
+            result = self.runprogram(["-outfile", "testgraph.html", "-s", "example.tex"])
+            self.assertEqual(result.returncode, 0)
             self.assertTrue(os.path.isfile("testgraph.html"))
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test with sourcefile and outfile flag set and arguments given for both (outfile without .html)
@@ -111,12 +118,12 @@ class TestParserforDependencis(unittest.TestCase):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-outfile testgraph -s example.tex"])
-            self.assertEqual(result.returncode, 2)      # Test bekommt 2 zurück, warum?
+            result = self.runprogram(["-outfile", "testgraph", "-s", "example.tex"])
+            self.assertEqual(result.returncode, 0)
             self.assertTrue(os.path.isfile("testgraph.html"))
-            print(f"{functionname}: SUCCESS")
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test with sourcefile flag but argument given not .tex
@@ -124,11 +131,12 @@ class TestParserforDependencis(unittest.TestCase):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-s example"])
-            self.assertEqual(result.returncode, 2)
-            print(f"{functionname}: SUCCESS")
+            result = self.runprogram(["-s", "example"])
+            self.assertEqual(result.returncode, 1)
+            self.assertEqual("Sourcefile is not an .tex file. \n\nPlease try again\n\n", result.stdout)
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
     # Test with a sourcefile that has no documentclass
@@ -136,24 +144,37 @@ class TestParserforDependencis(unittest.TestCase):
         functionname = inspect.currentframe().f_code.co_name
         print(f"Running test: {functionname}")
         try:
-            result = self.runprogram(["-s noclassexample.tex"])
-            self.assertEqual(result.returncode, 1)  # Test bekommt 1 zurück, warum?
-            print(f"{functionname}: SUCCESS")
+            result = self.runprogram(["-s", "noclassexample.tex"])
+            self.assertEqual(result.returncode, 1)
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
         except AssertionError as e:
-            print(f"{functionname}: FAILED")
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
             raise e
 
-    #def test_withbrowserflagsetwithnoneboolen(self):
-    #    functionname = inspect.currentframe().f_code.co_name
-    #    print(f"Running test: {functionname}")
-    #    try:
-    #        result = self.runprogram(["-s example.tex -b Test"])
-    #        self.assertEqual(result.returncode, 2)
-    #        self.assertIn("usage: ParserforDependencies.py [-h] [-outfile OUTFILENAME] [-s SOURCEFILE] [-b]", result.stderr)
-    #        print(f"{functionname}: SUCCESS")
-    #    except AssertionError as e:
-    #        print(f"{functionname}: FAILED")
-    #        raise e
+    def test_withbrowserflagsetwithnoneboolen(self):
+        functionname = inspect.currentframe().f_code.co_name
+        print(f"Running test: {functionname}")
+        try:
+            result = self.runprogram(["-s", "example.tex", "-b", "Test"])
+            self.assertEqual(result.returncode, 2)
+            self.assertIn('usage: ParserforDependenciesV2.py [-h] [-outfile OUTFILENAME] [-s SOURCEFILE]\n', result.stderr)
+            print(f"{functionname}: {Bcolors.OKGREEN}SUCCESS{Bcolors.ENDC}")
+        except AssertionError as e:
+            print(f"{functionname}: {Bcolors.FAIL}FAILED{Bcolors.ENDC}")
+            raise e
+
+    def tearDown(self):
+        try:
+            os.remove("graph.html")
+            print("Cleanup of graph.html successfull")
+        except OSError:
+            pass
+
+        try:
+            os.remove("testgraph.html")
+            print("Cleanup of testgraph.html successfull")
+        except OSError:
+            pass
 
 
 if __name__ == "__main__":
